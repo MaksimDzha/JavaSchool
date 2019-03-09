1c0c0f814d1c434bfba863f53f015553a8fbb7c9
MaksimDzha <junny@list.ru>
2019-03-10 00:48:24 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,4 +1,9 @@
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Person implements Comparable<Person> {
     private final boolean man;
     private final String name;
     private Person spouse;
@@ -6,6 +11,10 @@ public class Person {
     public Person(boolean man, String name) {
         this.man = man;
         this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     /**
@@ -16,7 +25,7 @@ public class Person {
      * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
      */
     public boolean marry(Person person) {
-        if (person == null){
+        if (person == null) {
             System.out.println("Person is empty");
             return false;
         }
@@ -63,11 +72,25 @@ public class Person {
         }
     }
 
+
+    @Override
+    public int compareTo(Person o) {
+        return this.getName().compareTo(o.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Person p = (Person) o;
+        if (this.getName() == p.getName()) return true;
+        else return false;
+    }
+
     public static void main(String[] args) {
         Person ivan = new Person(true, "Ivan");
         Person petr = new Person(true, "Petr");
         Person rita = new Person(false, "Rita");
         Person vika = new Person(false, "Vika");
+        Person ivan2 = new Person(true, "Ivan");
 
         ivan.marry(ivan);
         petr.divorce();
@@ -77,5 +100,17 @@ public class Person {
         petr.marry(null);
         petr.marry(new Person(false, "Irina"));
         petr.marry(rita);
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(ivan);
+        personList.add(ivan2);
+        personList.add(petr);
+        personList.add(rita);
+        personList.add(vika);
+        for (Person p: personList) System.out.println(p.getName());
+        System.out.println();
+
+        Set<Person> personSet = new TreeSet<Person>(personList);
+        for (Person p: personSet) System.out.println(p.getName());
     }
 }
